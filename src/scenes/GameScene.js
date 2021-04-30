e572afdc278a1d2f7cd9e97d0abfab7a99743750
evelynfredin <evefre2001@skola.goteborg.se>
2021-04-30 15:15:20 +0200

    added jumping feature

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,62 +1,86 @@
 import Phaser from 'phaser';
+import { createAligned } from './utils';
 
 const DUDE_KEY = 'dude';
 const GROUND_KEY = 'ground';
 
-export default class GameScene extends Phaser.Scene {
+export default class GameTestingScene extends Phaser.Scene {
   constructor() {
-    super();
+    super('game');
     this.player = undefined;
-    this.cursors = undefined;
+  }
+
+  init() {
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   preload() {
-    this.load.image('sky', 'src/assets/sky.png');
+    this.load.image('sky', 'src/assets/sky-warm.png');
+    this.load.image('mountains1', 'src/assets/mountains1.png');
+    this.load.image('mountains2', 'src/assets/mountains2.png');
+    this.load.image('plateau', 'src/assets/hills.png');
+    this.load.image('grass1', 'src/assets/grass1.png');
+    this.load.image('grass2', 'src/assets/grass2.png');
+    this.load.image(GROUND_KEY, 'src/assets/ground.png');
+
     this.load.spritesheet(DUDE_KEY, 'src/assets/dude.png', {
       frameWidth: 32,
       frameHeight: 48,
     });
-    this.load.image(GROUND_KEY, 'src/assets/platform.png');
   }
 
   create() {
-    this.createSky();
-    this.cursors = this.input.keyboard.createCursorKeys();
+    this.cameras.main.setBounds(0, 0, 1920 * 2, 600);
+    this.physics.world.setBounds(0, 0, 1920 * 2, 600);
+
+    this.cameras.main.setZoom(1);
+    const width = this.scale.width;
+    const height = this.scale.height;
+    const totalWidth = width * width;
+    this.add.image(width * 0.5, height * 0.5, 'sky').setScrollFactor(0);
+
+    createAligned(this, totalWidth, 'mountains1', 0.3);
+    createAligned(this, totalWidth, 'mountains2', 0.15);
+    createAligned(this, totalWidth, 'plateau', 0.5);
+    createAligned(this, totalWidth, 'grass1', 0.1116);
     this.createPlayer();
-    this.createCams();
     const platforms = this.createPlatforms();
+
     this.physics.add.collider(this.player, platforms);
   }
 
-  createSky() {
-    const sky = this.physics.add.staticGroup();
-    sky.create(400, 568, 'sky').setScale(2).refreshBody().setScrollFactor(0);
-  }
-
-  createCams() {
-    this.cameras.main.startFollow(this.player);
-    const speed = 5;
-
-    // if (this.cursors.right.isDown) {
-    //   cam.scrollX += speed;
-    // }
-    // if (this.cursors.left.isDown) {
-    //   cam.scrollX -= speed;
-    // }
-  }
-
   createPlatforms() {
+    const width = this.scale.width;
+    const height = this.scale.height;
     const platforms = this.physics.add.staticGroup();
 
-    platforms.create(400, 568, GROUND_KEY).setScale(2).refreshBody();
-
+    platforms
+      .create(800, 568, GROUND_KEY, 1)
+      .setScale(1)
+      .refreshBody()
+      .setScrollFactor(1);
+    platforms
+      .create(1500, 568, GROUND_KEY, 1)
+      .setScale(1)
+      .refreshBody()
+      .setScrollFactor(1);
+    platforms
+      .create(3000, 568, GROUND_KEY, 1)
+      .setScale(1)
+      .refreshBody()
+      .setScrollFactor(1);
     return platforms;
   }
 
   update() {
+    const cam = this.cameras.main;
+    const speed = 5;
+
     let velX = 0.0;
     const velY = -330;
     let anim = 'turn';
+
+    this.physics.add.collider(this.player, 'ground');
 
     if (this.cursors.left.isDown) {
       velX = -160;
@@ -70,17 +94,20 @@ export default class GameScene extends Phaser.Scene {
     if (this.cursors.up.isDown && this.player.body.touching.down) {
       this.player.setVelocityY(velY);
     }
-
     this.player.setVelocityX(velX);
     this.player.anims.play(anim, true);
-
-    // console.log({ x: this.player.x, y: this.player.y });
+    if (this.cursors.right.isDown) {
+      cam.scrollX += speed;
+    } else if (this.cursors.left.isDown) {
+      cam.scrollX -= speed;
+    }
   }
 
   createPlayer() {
-    this.player = this.physics.add.sprite(100, 450, DUDE_KEY);
+    this.player = this.physics.add
+      .sprite(100, 450, DUDE_KEY)
+      .setCollideWorldBounds(true);
     this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
     this.player.setScrollFactor(1);
 
     this.anims.create({
@@ -102,5 +129,7 @@ export default class GameScene extends Phaser.Scene {
       frameRate: 10,
       repeat: -1,
     });
+
+    this.cameras.main.startFollow(this.player, true, 0.08, 0.08);
   }
 }
